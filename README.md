23f90531316a434577cb25ea38a2bf73cee19ba2
Lyudmyla Kalmykova <omnipotence8.45@gmail.com>
2023-02-28 13:55:43 -1000

    First Test
    First Test

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Testing Talks Hub React App
+# react-app
---
+++ e2e/scr/step-definitions/assertion/verify-element-visibility.ts
@@ -0,0 +1,14 @@
+import {Then} from "@cucumber/cucumber";
+import { expect } from '@playwright/test'
+
+Then(
+    /^the contacts header should contain the text Contacts$/,
+    async function f() {
+
+        console.log("the contacts header should contain the text Contacts")
+
+        const content = await global.page.textContent("[data-id='contacts']")
+
+        expect(content).toBe('Contacts')
+    }
+)
